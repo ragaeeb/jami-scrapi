2ef593bd99d50bce448a7416e6e5cbb6e1e8a15a
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-11-16 16:48:39 -0500

    Address coderabbit comments

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -5,10 +5,7 @@
  * @returns The snake_case version of the provided text.
  */
 export const toSnakeCase = (camelCase: string) => {
-    return camelCase
-        .match(/([A-Z])/g)
-        ?.reduce((str, c) => str.replace(new RegExp(c), `_${c.toLowerCase()}`), camelCase)
-        .substring(camelCase.slice(0, 1).match(/([A-Z])/g) ? 1 : 0);
+    return camelCase.replace(/[A-Z]/g, (letter, index) => (index > 0 ? '_' : '') + letter.toLowerCase());
 };
 
 /**
